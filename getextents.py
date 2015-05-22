a5c794c6981af3076afd529a0a9e8a932c870241
eckardm <eckardm@umich.edu>
2015-05-22 15:03:21 -0400

    adding getextents

---
+++ getextents.py
@@ -0,0 +1,50 @@
+# import what we need
+import lxml
+from lxml import etree
+import csv
+import os
+from os.path import join
+
+# where are the eads?
+ead_path = 'path/to/EADs' # <-- you have to change this
+
+# where is the output csv?
+output_csv = 'path/to/output.csv' # <-- you have to change this
+
+# "top level" extents xpath
+extents_xpath = '//ead/archdesc/did//physdesc/extent'
+# component extents xpath
+component_extents_xpath = '//ead/archdesc/dsc//physdesc/extent'
+# all extents xpath
+all_extents = '//extent'
+
+# open and write header row of csv
+with open(output_csv, 'ab') as csv_file:
+    writer = csv.writer(csv_file, dialect='excel')
+    writer.writerow(['Filename', 'XPath', 'Original Extent'])
+
+# creates a function to get extents
+def getextents(xpath):
+    # go through those files
+    for filename in os.listdir(ead_path):
+        tree = etree.parse(join(ead_path, filename))
+        # keep up with where we are
+        print "Processing ", filename
+        # parse and go through all component extents
+        extents = tree.xpath(xpath)
+        for i in extents:
+            # identify blank extents
+            extent = i.text
+            extent_path = tree.getpath(i)
+            with open(output_csv, 'ab') as csvfile:
+                writer = csv.writer(csvfile, dialect='excel')
+                try:
+                    writer.writerow([filename, extent_path, extent])
+                except:
+                    writer.writerow([filename, extent_path, 'ISSUE EXTENT'])
+
+# close the csv
+csvfile.close()
+                
+# get extents       
+getextents(all_extents) # <-- you'll have to change this to get the extents you want, "top level," component level or all (i want all)
