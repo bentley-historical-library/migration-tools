b8dae75cef4813cd39a399ac884e483a9e3ee1df
eckardm <eckardm@umich.edu>
2015-07-08 17:24:25 -0400

    doing the deleting and replacing in the right order

--- ead/viaf/putname.py
+++ ead/viaf/putname.py
@@ -19,14 +19,14 @@ xml = re.compile('\.xml$')
 # controlaccess xpath
 controlaccess_xpath = '//ead/archdesc//controlaccess/*'
 
-print 'Updating EADs.'
+print 'Deleting existing authfileno attributes.'
 
 # go through the files
-for filename in os.listdir(ead_path_production):
+for filename in os.listdir(ead_path_test):
     # only look at xml
     if xml.search(filename):
         # parse
-        ead_tree = ET.parse(join(ead_path_production, filename))
+        ead_tree = ET.parse(join(ead_path_test, filename))
         # go through the eads
         for sub in ead_tree.xpath(controlaccess_xpath):
             print '\rWorking on it... |',
@@ -38,29 +38,57 @@ for filename in os.listdir(ead_path_production):
             print '\rWorking on it... -',
             print '\rWorking on it... -',
             print '\rWorking on it... \\',
-            # persname
+            # delete persname
             if sub.tag == 'persname' and 'authfilenumber' in sub.attrib:
                 del sub.attrib["authfilenumber"]
+                outfile = open(join(ead_path_test, filename), 'w')
+                outfile.write(ET.tostring(ead_tree, encoding="utf-8", xml_declaration=True))
+                outfile.close()
+            # delete corpname 
+            if sub.tag == 'corpname' and 'authfilenumber' in sub.attrib:
+                del sub.attrib["authfilenumber"]
+                outfile = open(join(ead_path_test, filename), 'w')
+                outfile.write(ET.tostring(ead_tree, encoding="utf-8", xml_declaration=True))
+                outfile.close()
+                
+print '\rExisting authfilenumber attributes deleted.'
+
+print 'Adding new authfilenumber attributes.'
+
+# go through the files
+for filename in os.listdir(ead_path_test):
+    # only look at xml
+    if xml.search(filename):
+        # parse
+        ead_tree = ET.parse(join(ead_path_test, filename))
+        # go through the eads
+        for sub in ead_tree.xpath(controlaccess_xpath):
+            print '\rWorking on it... |',
+            print '\rWorking on it... /',
+            print '\rWorking on it... -',
+            print '\rWorking on it... \\',
+            print '\rWorking on it... |',
+            print '\rWorking on it... /',
+            print '\rWorking on it... -',
+            print '\rWorking on it... -',
+            print '\rWorking on it... \\',
             if sub.tag == 'persname' and sub.text is not None and '--' not in sub.text:
                 original = sub.text.strip()
                 if original in persnames_dictionary:
                     persname_xpath = ead_tree.getpath(sub)
                     persname_update = ead_tree.xpath(persname_xpath)
                     persname_update[0].attrib['authfilenumber'] = persnames_dictionary[original]
-                    outfile = open(join(ead_path_production, filename), 'w')
+                    outfile = open(join(ead_path_test, filename), 'w')
                     outfile.write(ET.tostring(ead_tree, encoding="utf-8", xml_declaration=True))
                     outfile.close()
-            # corpname 
-            if sub.tag == 'corpname' and 'authfilenumber' in sub.attrib:
-                del sub.attrib["authfilenumber"]
             if sub.tag == 'corpname' and sub.text is not None and '--' not in sub.text:
-                original = sub.text.strip()
+                original = sub.text.strip() 
                 if original in corpnames_dictionary:
                     corpname_xpath = ead_tree.getpath(sub)
                     corpname_update = ead_tree.xpath(corpname_xpath)
                     corpname_update[0].attrib['authfilenumber'] = corpnames_dictionary[original]
-                    outfile = open(join(ead_path_production, filename), 'w')
+                    outfile = open(join(ead_path_test, filename), 'w')
                     outfile.write(ET.tostring(ead_tree, encoding="utf-8", xml_declaration=True))
                     outfile.close()
                     
-print '\rUpdated EADs. Woo hoo!'
+print '\rNew authfilenumber attributes added'
