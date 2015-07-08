6162f501328a6671166068b2a43ed354ca245b4a
eckardm <eckardm@umich.edu>
2015-07-08 17:26:31 -0400

    production mode

--- ead/viaf/putname.py
+++ ead/viaf/putname.py
@@ -22,11 +22,11 @@ controlaccess_xpath = '//ead/archdesc//controlaccess/*'
 print 'Deleting existing authfileno attributes.'
 
 # go through the files
-for filename in os.listdir(ead_path_test):
+for filename in os.listdir(ead_path_production):
     # only look at xml
     if xml.search(filename):
         # parse
-        ead_tree = ET.parse(join(ead_path_test, filename))
+        ead_tree = ET.parse(join(ead_path_production, filename))
         # go through the eads
         for sub in ead_tree.xpath(controlaccess_xpath):
             print '\rWorking on it... |',
@@ -41,13 +41,13 @@ for filename in os.listdir(ead_path_test):
             # delete persname
             if sub.tag == 'persname' and 'authfilenumber' in sub.attrib:
                 del sub.attrib["authfilenumber"]
-                outfile = open(join(ead_path_test, filename), 'w')
+                outfile = open(join(ead_path_production, filename), 'w')
                 outfile.write(ET.tostring(ead_tree, encoding="utf-8", xml_declaration=True))
                 outfile.close()
             # delete corpname 
             if sub.tag == 'corpname' and 'authfilenumber' in sub.attrib:
                 del sub.attrib["authfilenumber"]
-                outfile = open(join(ead_path_test, filename), 'w')
+                outfile = open(join(ead_path_production, filename), 'w')
                 outfile.write(ET.tostring(ead_tree, encoding="utf-8", xml_declaration=True))
                 outfile.close()
                 
@@ -56,11 +56,11 @@ print '\rExisting authfilenumber attributes deleted.'
 print 'Adding new authfilenumber attributes.'
 
 # go through the files
-for filename in os.listdir(ead_path_test):
+for filename in os.listdir(ead_path_production):
     # only look at xml
     if xml.search(filename):
         # parse
-        ead_tree = ET.parse(join(ead_path_test, filename))
+        ead_tree = ET.parse(join(ead_path_production, filename))
         # go through the eads
         for sub in ead_tree.xpath(controlaccess_xpath):
             print '\rWorking on it... |',
@@ -78,7 +78,7 @@ for filename in os.listdir(ead_path_test):
                     persname_xpath = ead_tree.getpath(sub)
                     persname_update = ead_tree.xpath(persname_xpath)
                     persname_update[0].attrib['authfilenumber'] = persnames_dictionary[original]
-                    outfile = open(join(ead_path_test, filename), 'w')
+                    outfile = open(join(ead_path_production, filename), 'w')
                     outfile.write(ET.tostring(ead_tree, encoding="utf-8", xml_declaration=True))
                     outfile.close()
             if sub.tag == 'corpname' and sub.text is not None and '--' not in sub.text:
@@ -87,7 +87,7 @@ for filename in os.listdir(ead_path_test):
                     corpname_xpath = ead_tree.getpath(sub)
                     corpname_update = ead_tree.xpath(corpname_xpath)
                     corpname_update[0].attrib['authfilenumber'] = corpnames_dictionary[original]
-                    outfile = open(join(ead_path_test, filename), 'w')
+                    outfile = open(join(ead_path_production, filename), 'w')
                     outfile.write(ET.tostring(ead_tree, encoding="utf-8", xml_declaration=True))
                     outfile.close()
                     
