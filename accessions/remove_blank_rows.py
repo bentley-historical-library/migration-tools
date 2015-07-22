a7f4fc0343264290a21f52e197b19b8a1e946096
eckardm <eckardm@umich.edu>
2015-07-22 13:15:32 -0400

    initial logic

--- accessions/remove_blank_rows.py
+++ accessions/remove_blank_rows.py
@@ -1,5 +1,5 @@
 '''
-From A-Team meeting on 2015-07-22...
+From A-Team meeting on 2015-07-22 regarding accessions without dates...
 
 Check core fields: if blank, then it's probably not a good record
   * Name
@@ -37,6 +37,8 @@ with open('C:/Users/Public/Documents/accessions/accessions-20150722-temp.csv','r
     for row in tqdm(csv_temp_take_two_reader, total=19253):
         # if there is no accessionid, the row is entirely blank
         accession_id = row[2]
+        # dates
+        accession_date = row[1]
         # core fields
         contact = []
         first_name = row[20]
@@ -57,11 +59,15 @@ with open('C:/Users/Public/Documents/accessions/accessions-20150722-temp.csv','r
         title = row[25]    
         if len(title) > 0:
             contact.append(title)
-        print contact
         accession_description = row[0]
         processing_status = row[35]
         if len(accession_id) == 0:
             continue
+        if len(accession_date) == 0:
+            if len(contact) == 0 and len(accession_description) == 0:
+                continue
+            if len(processing_status) == 0 or processing_status.strip() == 'Completed':
+                continue
         else:
             with open('C:/Users/Public/Documents/accessions/accessions-20150722-final.csv', 'ab') as csv_final_take_two:
                 csv_final_take_two_writer = csv.writer(csv_final_take_two, dialect='excel')
