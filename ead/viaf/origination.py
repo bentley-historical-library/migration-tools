d6f3a274933e4aa0242045db7f1541bd7fcb5aee
eckardm <eckardm@umich.edu>
2015-07-23 11:46:51 -0400

    removes IOError exception

--- ead/viaf/origination.py
+++ ead/viaf/origination.py
@@ -18,22 +18,18 @@ print 'Adding authority file numbers to <origination> tag when they exist in <co
 # go through the files
 for filename in tqdm(os.listdir(ead_path_test)):
     # parser = ET.XMLParser(remove_blank_text=True)
-    try:
-        if filename.endswith('.xml'):
-            ead_tree = ET.parse(join(ead_path_test, filename))
-            controlaccess_subelement_dictionary = {}
-            for controlaccess_subelement in ead_tree.xpath(controlaccess_xpath):
-                if 'authfilenumber' in controlaccess_subelement.attrib:
-                    controlaccess_subelement_dictionary[controlaccess_subelement.text] = controlaccess_subelement.get('authfilenumber')
-            for origination_subelement in ead_tree.xpath(origination_xpath):
-                if origination_subelement.text in controlaccess_subelement_dictionary:
-                    origination_subelement.set('authfilenumber', controlaccess_subelement_dictionary[origination_subelement.text])
-        outfile = open(join(ead_path_test, filename), 'w')
-        outfile.write(ET.tostring(ead_tree, pretty_print=True, encoding="utf-8", xml_declaration=True))
-        outfile.close()
-    except IOError:
-        print filename
-        continue
+    if filename.endswith('.xml'):
+        ead_tree = ET.parse(join(ead_path_test, filename))
+        controlaccess_subelement_dictionary = {}
+        for controlaccess_subelement in ead_tree.xpath(controlaccess_xpath):
+            if 'authfilenumber' in controlaccess_subelement.attrib:
+                controlaccess_subelement_dictionary[controlaccess_subelement.text] = controlaccess_subelement.get('authfilenumber')
+        for origination_subelement in ead_tree.xpath(origination_xpath):
+            if origination_subelement.text in controlaccess_subelement_dictionary:
+                origination_subelement.set('authfilenumber', controlaccess_subelement_dictionary[origination_subelement.text])
+    outfile = open(join(ead_path_test, filename), 'w')
+    outfile.write(ET.tostring(ead_tree, pretty_print=True, encoding="utf-8", xml_declaration=True))
+    outfile.close()
         
                 
 print '\rAdded authority file numbers to <origination> tag when they exist in <controlaccess> subfields.'
