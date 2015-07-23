26114438aa164567c51267060e3dadc2e58ac3e2
eckardm <eckardm@umich.edu>
2015-07-23 11:45:35 -0400

    adds authfilenumber to <origination> subelements, will wait for walker to normalize before running on vandura

---
+++ ead/viaf/origination.py
@@ -0,0 +1,41 @@
+# import what we need
+import os
+from os.path import join
+import csv
+from lxml import etree as ET
+from tqdm import *
+
+# where are the eads (test and production)?
+ead_path_test = 'C:/Users/Public/Documents/Real_Masters_all'
+ead_path_production = 'C:/Users/eckardm/GitHub/vandura/Real_Masters_all'
+
+# xpaths
+controlaccess_xpath = '//ead/archdesc//controlaccess/*'
+origination_xpath = '//ead/archdesc//origination/*'
+
+print 'Adding authority file numbers to <origination> tag when they exist in <controlaccess> subfields.'
+
+# go through the files
+for filename in tqdm(os.listdir(ead_path_test)):
+    # parser = ET.XMLParser(remove_blank_text=True)
+    try:
+        if filename.endswith('.xml'):
+            ead_tree = ET.parse(join(ead_path_test, filename))
+            controlaccess_subelement_dictionary = {}
+            for controlaccess_subelement in ead_tree.xpath(controlaccess_xpath):
+                if 'authfilenumber' in controlaccess_subelement.attrib:
+                    controlaccess_subelement_dictionary[controlaccess_subelement.text] = controlaccess_subelement.get('authfilenumber')
+            for origination_subelement in ead_tree.xpath(origination_xpath):
+                if origination_subelement.text in controlaccess_subelement_dictionary:
+                    origination_subelement.set('authfilenumber', controlaccess_subelement_dictionary[origination_subelement.text])
+        outfile = open(join(ead_path_test, filename), 'w')
+        outfile.write(ET.tostring(ead_tree, pretty_print=True, encoding="utf-8", xml_declaration=True))
+        outfile.close()
+    except IOError:
+        print filename
+        continue
+        
+                
+print '\rAdded authority file numbers to <origination> tag when they exist in <controlaccess> subfields.'
+
+# don't forget pretty print
