458c42cb45d7ea4de556d9d10b6e553140bff86e
eckardm <eckardm@umich.edu>
2015-07-23 15:53:36 -0400

    removes duplicates from csv

--- ead/agents/getsubjects.py
+++ ead/agents/getsubjects.py
@@ -7,6 +7,11 @@ from tqdm import *
 # where are the eads?
 ead_path = 'C:/Users/eckardm/GitHub/vandura/Real_Masters_all'
 
+# where are the temp files?
+persname_temp = 'persname_temp.csv'
+famname_temp = 'famname_temp.csv'
+corpname_temp = 'corpname_temp.csv'
+
 # where are the output files?
 persname_output = 'persname.csv'
 famname_output = 'famname.csv'
@@ -20,12 +25,30 @@ corpname_headers = ['Type', 'Publish', 'Authority ID', 'Source', 'Rules', 'ORIGI
 # empty list
 list = []
 
+# xpaths
+origination_xpath =  '//origination/*'
+controlaccess_xpath = '//controlaccess/*'
+
 print 'Creating dictionary...'
 
 for filename in tqdm(os.listdir(ead_path)):
     if filename.endswith('.xml'):
         ead_tree = ET.parse(join(ead_path, filename))
-        for controlaccess_subelement in ead_tree.xpath('//*'):
+        for controlaccess_subelement in ead_tree.xpath(origination_xpath):
+            if (controlaccess_subelement.tag == 'persname' or controlaccess_subelement.tag == 'famname' or controlaccess_subelement.tag == 'corpname') and controlaccess_subelement.text is not None and '--' not in controlaccess_subelement.text:
+                dictionary = {}
+                dictionary['Type'] = controlaccess_subelement.tag
+                dictionary['ORIGINAL'] = controlaccess_subelement.text.strip()
+                if 'authfilenumber' in controlaccess_subelement.attrib:
+                    dictionary['Authority ID'] = controlaccess_subelement.get('authfilenumber')
+                if 'source' in controlaccess_subelement.attrib:
+                    dictionary['source'] = controlaccess_subelement.get('source')
+                list.append(dictionary)
+                
+for filename in tqdm(os.listdir(ead_path)):
+    if filename.endswith('.xml'):
+        ead_tree = ET.parse(join(ead_path, filename))
+        for controlaccess_subelement in ead_tree.xpath(controlaccess_xpath):
             if (controlaccess_subelement.tag == 'persname' or controlaccess_subelement.tag == 'famname' or controlaccess_subelement.tag == 'corpname') and controlaccess_subelement.text is not None and '--' not in controlaccess_subelement.text:
                 dictionary = {}
                 dictionary['Type'] = controlaccess_subelement.tag
@@ -38,7 +61,7 @@ for filename in tqdm(os.listdir(ead_path)):
             
 print 'Creating <persname> CSV...'
 
-with open(persname_output, 'wb') as persname_csv:
+with open(persname_temp, 'wb') as persname_csv:
     persname_header_writer = csv.writer(persname_csv)
     persname_header_writer.writerow(persname_headers)
     
@@ -54,16 +77,25 @@ for dictionary_item in tqdm(list):
         else:
             persname_row.append('')
         persname_row.append('')
-        persname_row.append('')
         persname_row.append('Indirect')
         persname_row.append(dictionary_item['ORIGINAL'].encode('utf-8'))
-        with open(persname_output, 'ab') as persname_csv_take_two:
+        with open(persname_temp, 'ab') as persname_csv_take_two:
             persname_writer = csv.writer(persname_csv_take_two)
             persname_writer.writerow(persname_row)
+            
+with open(persname_temp, 'rb') as persname_in, open(persname_output, 'wb') as persname_out:
+    persnames = set()
+    for row in persname_in:
+        if row in persnames: 
+            continue
+        persnames.add(row)
+        persname_out.write(row)
+
+os.remove(persname_temp)
         
 print 'Creating <famname> CSV...'
         
-with open(famname_output, 'wb') as famname_csv:
+with open(famname_temp, 'wb') as famname_csv:
     famname_header_writer = csv.writer(famname_csv)
     famname_header_writer.writerow(famname_headers)
     
@@ -80,13 +112,23 @@ for dictionary_item in tqdm(list):
             famname_row.append('')
         famname_row.append('')
         famname_row.append(dictionary_item['ORIGINAL'].encode('utf-8'))
-        with open(famname_output, 'ab') as famname_csv_take_two:
+        with open(famname_temp, 'ab') as famname_csv_take_two:
             famname_writer = csv.writer(famname_csv_take_two)
             famname_writer.writerow(famname_row) 
+            
+with open(famname_temp, 'rb') as famname_in, open(famname_output, 'wb') as famname_out:
+    famnames = set()
+    for row in famname_in:
+        if row in famnames: 
+            continue
+        famnames.add(row)
+        famname_out.write(row)
+
+os.remove(famname_temp)
 
 print 'Creating <corpname> CSV...'
         
-with open(corpname_output, 'wb') as corpname_csv:
+with open(corpname_temp, 'wb') as corpname_csv:
     corpname_header_writer = csv.writer(corpname_csv)
     corpname_header_writer.writerow(corpname_headers)
     
@@ -103,9 +145,19 @@ for dictionary_item in tqdm(list):
             corpname_row.append('')
         corpname_row.append('')
         corpname_row.append(dictionary_item['ORIGINAL'].encode('utf-8'))
-        with open(corpname_output, 'ab') as corpname_csv_take_two:
+        with open(corpname_temp, 'ab') as corpname_csv_take_two:
             corpname_writer = csv.writer(corpname_csv_take_two)
             corpname_writer.writerow(famname_row)  
+            
+with open(corpname_temp, 'rb') as corpname_in, open(corpname_output, 'wb') as corpname_out:
+    corpnames = set()
+    for row in corpname_in:
+        if row in corpnames: 
+            continue
+        corpnames.add(row)
+        corpname_out.write(row)
+
+os.remove(corpname_temp)
             
 print 'Done!'
                     
