89fde5b1e73ef5413c8361f940c13fa20e9b5c8d
Walker Boyle <wboyle13@gmail.com>
2015-07-24 15:34:51 -0400

    convert tabs to spaces
    displays better on github with spaces instead of tabs

--- ead/viaf/getname.py
+++ ead/viaf/getname.py
@@ -19,27 +19,27 @@ tag_names = ["persname", "corpname", "geogname", "genreform"]
 # initializes the dictionary we'll use to hold the tag names
 controlaccess_term_dictionary = {}
 for tag in tag_names:
-	controlaccess_term_dictionary[tag] = []
+    controlaccess_term_dictionary[tag] = []
 
 # xpath to where in the ead document we'll be looking for the given tag names
 controlaccess_xpath = '//controlaccess/*'
 
 # go through the files
 for filename in tqdm(os.listdir(ead_path)):
-	# only look at xml files
-	if filename.endswith(".xml"):
-		# create lxml etree version of ead document
-		ead_tree = ET.parse(path.join(ead_path, filename))
+    # only look at xml files
+    if filename.endswith(".xml"):
+        # create lxml etree version of ead document
+        ead_tree = ET.parse(path.join(ead_path, filename))
 
-		# go through the ead and grab all the text appearing in the tag types defined above
-		for sub in ead_tree.xpath(controlaccess_xpath):
-			for tag in tag_names:
-			    # we don't want to grab compound subject terms or empty strings
-				if tag in sub.tag and sub.text is not None and "--" not in sub.text:
-					controlaccess_term_dictionary[tag].append(sub.text.strip())
+        # go through the ead and grab all the text appearing in the tag types defined above
+        for sub in ead_tree.xpath(controlaccess_xpath):
+            for tag in tag_names:
+                # we don't want to grab compound subject terms or empty strings
+                if tag in sub.tag and sub.text is not None and "--" not in sub.text:
+                    controlaccess_term_dictionary[tag].append(sub.text.strip())
 
 # write all the names to a file - one file for each tag type
 for tag_type, names in controlaccess_term_dictionary.items():
-	with open(tag_type + ".txt", mode="w") as text_file:
-		for name in names:
-			text_file.write(name.encode("utf-8") + '\n')
+    with open(tag_type + ".txt", mode="w") as text_file:
+        for name in names:
+            text_file.write(name.encode("utf-8") + '\n')
